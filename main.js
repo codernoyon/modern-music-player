7d4f37e1da46cb077d6705df1862f01445c6b5e3
codernoyon <noyonrahman2003@gmail.com>
2021-12-29 21:02:42 +0600

    all files load

---
+++ main.js
@@ -0,0 +1,11 @@
+function openP() {
+    const playList = document.querySelector('.playlist');
+    playList.classList.toggle('active');
+};
+
+function sidebar() {
+    const sidebar = document.querySelector('.options');
+    sidebar.classList.toggle('active');
+}
+
+
